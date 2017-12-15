72b50f5ded2f479acc2338394a9293a4e213a0e1
Alessandra Nishikawa <alessandranishikawa@imacdeaessandra.local>
2017-12-15 12:50:19 -0500

    version_finale

--- Evaluation+/CreatorController.swift
+++ Evaluation+/CreatorController.swift
@@ -1,43 +1,46 @@
 
+//--- Biblioteques du Xcode utilisées dans le code
 // -----------------------
 import UIKit
 import Foundation
 // -----------------------
 
+//---Class CreatorController pour contrôler la vues dans le code
 class CreatorController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
    
     //----------------------
-    
+    //--- Constante qui utilisera les fonctions du UserDefaultsManager()
     let saveDataObj  = UserDefaultsManager()
     
+    //--- Outlet qui va lier à label nom de èléve et lier champs textField de course et grade
     //---------------------
-   
     @IBOutlet weak var student_name_label: UILabel!
     @IBOutlet weak var course_field: UITextField!
     @IBOutlet weak var grade_field: UITextField!
     
-   //----------------------
- 
+    //--- Declaration de la tableview qui va garder l'informations du èléves
+    //----------------------
     @IBOutlet weak var criteriaTableView: UITableView!
     
+    //--- Declaration de champ pour la moyenne d'èléve
     //----------------------
-    
     @IBOutlet weak var average: UILabel!
     
+    //--- Typealias qui renommeront les variables
     //-----------------------
-   
     typealias studentName = String
     typealias courseName = String
     typealias gradeCourse = Double
    
-   //----------------------
-    
+    //--- Tableaux qui contient le nom, le cours et la note de l'élève
+    //----------------------
     var studentGrades: [studentName: [courseName: gradeCourse]]!
     var arrayOfCourses: [courseName]!
     var arrayOfGrades: [gradeCourse]!
     
+    //---------------------
+    //--- Section viewDidLoad pour initialisation supplémentaire sur les vues qui ont été chargées
     //----------------------
-   
     override func viewDidLoad(){
         super.viewDidLoad()
         student_name_label.text = saveDataObj.getValue(theKey: "name") as? String
@@ -46,22 +49,21 @@ class CreatorController: UIViewController, UITableViewDelegate, UITableViewDataS
         average.text = verifierAverage(dictionnaireNotes: moyenneNotes(), ruleOfThree: {$0 * 100.0 / $1})
     }
     
+    //--- Fonction tableview qui indique à la source de données de renvoyer le nombre de lignes dans une section donnée d'une vue de table. Retourne à studentGrades
     //----------------------
-   
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return arrayOfCourses.count
         
     }
     
+    //--- Fonction qui demande la source de données pour une cellule à insérer dans un emplacement particulier de la vue de table
     //---------------------
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = criteriaTableView.dequeueReusableCell(withIdentifier: "criteriaCell")!
         
         if let aCourse = cell.viewWithTag(100) as! UILabel! {
             aCourse.text = arrayOfCourses[indexPath.row]
         }
-        //Afficher les elements de le tableau
         if let aGrade = cell.viewWithTag(101) as! UILabel! {
             aGrade.text = String(arrayOfGrades[indexPath.row])
         }
@@ -69,6 +71,7 @@ class CreatorController: UIViewController, UITableViewDelegate, UITableViewDataS
     }
  
     //------ Fonction pour effacer la grade et note
+    //---------------------
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == UITableViewCellEditingStyle.delete {
             let name = student_name_label.text
@@ -86,8 +89,8 @@ class CreatorController: UIViewController, UITableViewDelegate, UITableViewDataS
         }
     }
     
+    //--- Function pour garder les informations d'èléve
     //---------------------
-    
     func loadSavedData() {
         if saveDataObj.doesKeyExist(theKey: "gradeCourse") {
             studentGrades = saveDataObj.getValue(theKey: "gradeCourse") as! [studentName: [courseName: gradeCourse]]
@@ -96,8 +99,8 @@ class CreatorController: UIViewController, UITableViewDelegate, UITableViewDataS
         }
     }
     
+    //--- Bounton pour ajouter les informations disciplines et notes d'élève
     //---------------------
-    
     @IBAction func addCriteriaButton(_ sender: UIButton) {
         let name = student_name_label.text!
         
@@ -113,6 +116,7 @@ class CreatorController: UIViewController, UITableViewDelegate, UITableViewDataS
 
     }
 
+    //--- Fonction qui remplit la table avec les informations saisies par l'utilisateur
     //---------------------
     func saveData() {
         let name = student_name_label.text
@@ -121,7 +125,7 @@ class CreatorController: UIViewController, UITableViewDelegate, UITableViewDataS
         arrayOfGrades = [gradeCourse](courses_and_grades!.values)
     }
     
-    //------------------ Function Average
+    //---Fonction Average pour consulter la moyenne
     func verifierAverage(dictionnaireNotes: [Double: Double], ruleOfThree: (_ somme: Double, _ sur: Double) -> Double) -> String{
         
         let sommeNotes = [Double](dictionnaireNotes.keys).reduce(0, +)
@@ -131,16 +135,16 @@ class CreatorController: UIViewController, UITableViewDelegate, UITableViewDataS
         
     }
     
+    //--- Fonction qui calcule les notes et les moyennes des étudiants
     //-----------------
-    
     func averageNotes(tableauDeNotes: [Double], moyenne: (_ sum: Double, _ nombreDeNotes: Double) -> Double) -> Double {
         let somme = tableauDeNotes.reduce(0, +)
         let resultat = moyenne(somme, Double(tableauDeNotes.count ))
         return resultat
     }
     
+    // Calcule la moyenne
     //-----------------
-    
     func moyenneNotes() ->  [Double: Double] {
         let average = arrayOfGrades.reduce(0, +)
         let somme = arrayOfGrades.count
@@ -149,7 +153,8 @@ class CreatorController: UIViewController, UITableViewDelegate, UITableViewDataS
         return dictionnaireGrades
     }
 
-    //------ Fonction pour clavier
+    //--- Fonction pour déclencher l'activité de retour du clavier
+    //--------------------
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
     }
--- Evaluation+/MainController.swift
+++ Evaluation+/MainController.swift
@@ -5,51 +5,60 @@
 //  Created by eleves on 17-11-20.
 //  Copyright © 2017 eleves. All rights reserved.
 //
+//--- Biblioteques du Xcode utilisées dans le code
 //---------------------
 import UIKit
 import Foundation
 //---------------------
+
+//---Class viewController pour contrôler la vues dans le code
+//---------------------
 class MainController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
 
+    //--- Outlet qui va lier à tableView et textField
     //---------------------
     @IBOutlet weak var student_name_tableview: UITableView!
     @IBOutlet weak var student_name_field: UITextField!
     //---------------------
     
+    //--- Constante qui utilisera les fonctions du UserDefaultsManager()
     let saveDataObj = UserDefaultsManager()
     
+    //--- Typealias qui renommeront les variables
     //---------------------
     typealias studentName = String
     typealias courseName = String
     typealias gradeCourse = Double
     //---------------------
     
+    //--- Tableau de dictionaires studentGrades qui contient le nom, le cours et la note de l'élève
+    //---------------------
     var studentGrades: [studentName: [courseName: gradeCourse]]!
     
+    //--- Section viewDidLoad pour initialisation supplémentaire sur les vues qui ont été chargées
     //---------------------
-    
     override func viewDidLoad() {
             super.viewDidLoad()
             loadUserDefaults()
     }
     
+    //--- Fonction tableview qui indique à la source de données de renvoyer le nombre de lignes dans une section donnée d'une vue de table. Retourne à studentGrades
     //---------------------
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             return studentGrades.count
     }
     
+    //--- Fonction qui demande la source de données pour une cellule à insérer dans un emplacement particulier de la vue de table
     //---------------------
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default,
                                                         reuseIdentifier: nil)
             cell.textLabel?.text = [studentName](studentGrades.keys)[indexPath.row]
             return cell
     }
-    
-    //------ Fonction pour effacer
-    
+ 
+    //--- Fonction pour effacerla cell du tableView. Le contrôle d'édition utilisé par une cellule
+    //---------------------
     func tableView(_ tableView: UITableView, commit editinsStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
             if editinsStyle == UITableViewCellEditingStyle.delete {
                 let name = [studentName](studentGrades.keys)[indexPath.row]
@@ -59,23 +68,23 @@ class MainController: UIViewController, UITableViewDelegate, UITableViewDataSour
             }
     }
     
-    //------ Fonction pour continuer dans la deuxieme page
-    
+    //--- Fonction pour continuer dans la deuxieme page, le indentifier est la nom "seg"
+    //---------------------
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let name = [studentName](studentGrades.keys)[indexPath.row]
             saveDataObj.setKey(theValue: name as AnyObject, theKey: "name")
             performSegue(withIdentifier: "seg", sender: nil)
     }
     
-   //---------------------
-    
+    //--- Renvoyer la valeur du textField comme vraie
+    //---------------------
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
             textField.resignFirstResponder()
             return true
     }
     
+    //--- Function pour garder les qui contient le nom, le cours et la note de l'élève
     //---------------------
-    
     func loadUserDefaults() {
         if saveDataObj.doesKeyExist(theKey: "gradeCourse") {
                 studentGrades = saveDataObj.getValue(theKey: "gradeCourse") as! [studentName: [courseName: gradeCourse]]
@@ -84,8 +93,9 @@ class MainController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
-    //---------------------
     
+    //--- Bounton pour ajouter les informations d'élève
+    //---------------------
     @IBAction func addstudent(_ sender: UIButton) {
         if student_name_field.text != "" {
             studentGrades[student_name_field.text!] = [courseName: gradeCourse]()
@@ -95,8 +105,8 @@ class MainController: UIViewController, UITableViewDelegate, UITableViewDataSour
             }
         }
     
-    //------ Fonction pour clavier
-    
+    //--- Fonction pour déclencher l'activité de retour du clavier
+    //--------------------
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
             view.endEditing(true)
     }
